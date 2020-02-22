d506535611b9ac7550fda5d98521d3a032be305d
“binfeng <469440791@qq.com>
2020-02-22 16:45:09 +0800

    Site updated: 2020-02-22 16:45:05

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -1090,7 +1090,7 @@ var home = location.href,
     },
     AB: function () {
       if (window.location.pathname.indexOf('about') > -1) {
-        $.getScript('/js/botui.js', function () {
+        $.getScript('https://cdn.jsdelivr.net/gh/moezx/cdn@latest/js/botui/botui.js', function () {
           if (typeof(botui) == undefined && !botui.message) {
             bot_ui_ini()
           }
